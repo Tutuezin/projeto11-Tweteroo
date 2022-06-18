adb01cb0ab06fe85d70c0e7938bdd21585af66f8
Tutuezin <vinnycamara1@gmail.com>
2022-06-17 23:29:27 -0300

    :sparkles: Feat: post sign-up -> OK!

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,29 @@
 import express from "express";
 import cors from "cors";
+import chalk from "chalk";
 
 const app = express();
+app.use([cors(), express.json()]);
 
-app.use(cors());
+const users = [];
+const tweets = [];
+
+app.get("/sign-up", (req, res) => {
+  res.send(users);
+});
+
+app.post("/sign-up", (req, res) => {
+  const user = req.body;
+  users.push(user);
+
+  console.log(user);
+  res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+  res.send(tweets);
+});
+
+app.listen(5000, () => {
+  console.log(chalk.bold.green("Rodando..."));
+});
